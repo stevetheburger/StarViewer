9312cda994382e76c61c7b00a745b25973010c3a
Stephen Hyberger <stephenhyberger@outlook.com>
2025-10-06 21:55:26 -0500

    StarViewer v0.0.0: Added missing javadoc content. -Fixed task.json to name the folders that contain .java files explicitly. Javadoc should be able to do this itself but it doesn't seem to be happening.

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"Data"},{"l":"Logging"}];updateSearchResults();
